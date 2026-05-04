7609e3022460e80793038059d39164fc1bbfa90d
Hosein-tehrani <ho3einvictorho3ein@gmail.com>
2026-05-05 01:11:17 +0330

    adding photos  + omprove card style

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,14 @@
-
 import { RotatingLines } from "react-loader-spinner";
 import styles from "./Loader.module.css";
-function Loader() { 
+function Loader() {
   return (
     <div className={styles.loader}>
-        <RotatingLines width="100px" height="100px" strokeWidth="3" strokeColor="#fe5d42" />
+      <RotatingLines
+        width="100px"
+        height="100px"
+        strokeWidth="3"
+        strokeColor="var(--primary)"
+      />
     </div>
   );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,34 +1,49 @@
-
 import { TbListDetails, TbShoppingBagCheck } from "react-icons/tb";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { productQuantity, shortenText } from "../helper/helper";
 import styles from "./ProductCard.module.css";
 import { MdDeleteOutline } from "react-icons/md";
 import { useSelector, useDispatch } from "react-redux";
-import { addItem, decrease, increase, removeItem } from "../features/cart/cartSlice";
+import {
+  addItem,
+  decrease,
+  increase,
+  removeItem,
+} from "../features/cart/cartSlice";
 
+function ProductCard({ productData }) {
+  const { title, image, category, price, id } = productData;
+  const state = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  const quantity = productQuantity(state, id);
+  const editedPhoto = image.split("/")[4];
 
-function ProductCard({ productData }) { 
-    const {title, image, category, price, id} = productData
-    const state = useSelector(store => store.cart)
-    const dispatch = useDispatch()
-    const quantity = productQuantity(state, id)
   return (
     <div className={styles.card}>
-      <h3>{ shortenText(title) }</h3>
-      {/* <img src={image} alt={title} /> */}
-      <div className="details">
-        <div className="category">{category}</div>
-        <div className="price">${price}</div>
-      </div>
+      <Link to={"/product/" + id} className={styles.top}>
+        <img src={`productPhotos/${editedPhoto}`} alt={title} />
+        <h3>{shortenText(title)}</h3>
+      </Link>
       <div className={styles.actions}>
-        <Link to={"/product/"+id}><TbListDetails /></Link>
+        <div className={styles.price}>${price.toLocaleString()}</div>
         <div>
-            {quantity > 1 && <button onClick={()=> dispatch(decrease(productData))}>-</button>}
-            {quantity === 1 && <button onClick={()=> dispatch(removeItem(productData))}><MdDeleteOutline/></button>}
-            {!!quantity && quantity}
-            {quantity > 0 && <button onClick={()=> dispatch(increase(productData))}>+</button>}
-            {quantity === 0 && <button onClick={()=> dispatch(addItem(productData))}><TbShoppingBagCheck /></button>}
+          {quantity > 1 && (
+            <button onClick={() => dispatch(decrease(productData))}>-</button>
+          )}
+          {quantity === 1 && (
+            <button onClick={() => dispatch(removeItem(productData))}>
+              <MdDeleteOutline />
+            </button>
+          )}
+          {!!quantity && <span>{quantity}</span>}
+          {quantity > 0 && (
+            <button onClick={() => dispatch(increase(productData))}>+</button>
+          )}
+          {quantity === 0 && (
+            <button onClick={() => dispatch(addItem(productData))}>
+              <TbShoppingBagCheck />
+            </button>
+          )}
         </div>
       </div>
     </div>
