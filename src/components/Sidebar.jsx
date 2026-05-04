f3c3acf05c54414215753cf9c37903d5f0ea275f
Hosein-tehrani <ho3einvictorho3ein@gmail.com>
2026-05-05 01:13:41 +0330

    improve sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,30 @@
-
 const filters = [
-    {name: "all",value:"", id: 1},
-    {name: "Electronics",value:"electronics", id: 2},
-    {name: "Jewelery",value:"jewelery", id: 3},
-    {name: "Men's Clothing",value:"men's clothing", id: 4},
-    {name: "Women's Clothing",value:"women's clothing", id: 5},
-]
+  { name: "All", value: "", id: 1 },
+  { name: "Electronics", value: "electronics", id: 2 },
+  { name: "Jewelery", value: "jewelery", id: 3 },
+  { name: "Men's Clothing", value: "men's clothing", id: 4 },
+  { name: "Women's Clothing", value: "women's clothing", id: 5 },
+];
 import styles from "./Sidebar.module.css";
 import { FaListUl } from "react-icons/fa";
-function Sidebar({category,categoryHandler}) { 
-
+function Sidebar({ category, categoryHandler }) {
   return (
-    <div className={styles.Sidebar}>
-        <div>
-            <FaListUl/>
-            <p>Categories</p>
-        </div>
-        <ul>
-            {filters.map(list => <li onClick={() => categoryHandler(list.value)} key={list.id}>{list.name}</li>)}
-        </ul>
+    <div className={styles.sidebar}>
+      <div className={styles.title}>
+        <FaListUl />
+        <p>Categories</p>
+      </div>
+      <ul>
+        {filters.map((list) => (
+          <li
+            className={category === list.value ? styles.selected : undefined}
+            onClick={() => categoryHandler(list.value)}
+            key={list.id}
+          >
+            {list.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
