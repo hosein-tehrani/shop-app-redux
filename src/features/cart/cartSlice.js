4467537a0b5fc90524c137bda091065cd885f571
Hosein-tehrani <ho3einvictorho3ein@gmail.com>
2026-05-05 20:05:17 +0330

    fixs

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,55 +2,61 @@ import { createSlice } from "@reduxjs/toolkit";
 import { sumProducts } from "../../helper/helper";
 import { useCart } from "../../context/CartContext";
 const initialState = {
-    selectedProducts: [],
-    itemCounter: 0,
-    total: 0,
-    checkout: false
-}
+  selectedProducts: [],
+  itemCounter: 0,
+  total: 0,
+  checkout: false,
+};
 const cartSlice = createSlice({
-    name: "cart",
-    initialState,
-    reducers: {
-        addItem: (state, action)=> {
-            state.selectedProducts.push({...action.payload, quantity: 1})
-            const {total , itemCounter} = sumProducts(state.selectedProducts)
-            state.total = total;
-            state.itemCounter = itemCounter;
-            state.checkout = false;
-        },
-        removeItem: (state, action) => {
-            const newSelected = state.selectedProducts.filter(item => item.id !== action.payload.id)
-            state.selectedProducts = newSelected
-            console.log("state: ", state);
-            console.log("action: ", action);
-            
-            const {total , itemCounter} = sumProducts(state.selectedProducts)
-            state.total = total;
-            state.itemCounter = itemCounter;
-            state.checkout = false;
-        },
-        increase: (state, action) => {
-            const incIndex = state.selectedProducts.findIndex(item => item.id === action.payload.id)
-            state.selectedProducts[incIndex].quantity++
-            const {total , itemCounter} = sumProducts(state.selectedProducts)
-            state.total = total;
-            state.itemCounter = itemCounter;
-            state.checkout = false;
-        },
-        decrease: (state, action) => {
-            const decIndex = state.selectedProducts.findIndex(item => item.id === action.payload.id)
-            state.selectedProducts[decIndex].quantity--
-            const {total , itemCounter} = sumProducts(state.selectedProducts)
-            state.total = total;
-            state.itemCounter = itemCounter;
-            state.checkout = false;
-        }
-    }
-})
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.selectedProducts.push({ ...action.payload, quantity: 1 });
+      const { total, itemCounter } = sumProducts(state.selectedProducts);
+      state.total = total;
+      state.itemCounter = itemCounter;
+      state.checkout = false;
+    },
+    removeItem: (state, action) => {
+      const newSelected = state.selectedProducts.filter(
+        (item) => item.id !== action.payload.id,
+      );
+      state.selectedProducts = newSelected;
+
+      const { total, itemCounter } = sumProducts(state.selectedProducts);
+      state.total = total;
+      state.itemCounter = itemCounter;
+      state.checkout = false;
+    },
+    increase: (state, action) => {
+      const incIndex = state.selectedProducts.findIndex(
+        (item) => item.id === action.payload.id,
+      );
+      state.selectedProducts[incIndex].quantity++;
+      const { total, itemCounter } = sumProducts(state.selectedProducts);
+      state.total = total;
+      state.itemCounter = itemCounter;
+      state.checkout = false;
+    },
+    decrease: (state, action) => {
+      const decIndex = state.selectedProducts.findIndex(
+        (item) => item.id === action.payload.id,
+      );
+      state.selectedProducts[decIndex].quantity--;
+      const { total, itemCounter } = sumProducts(state.selectedProducts);
+      state.total = total;
+      state.itemCounter = itemCounter;
+      state.checkout = false;
+    },
+    checkout: (state) => {
+      state.selectedProducts = [];
+      state.total = 0;
+      state.itemCounter = 0;
+      state.checkout = true;
+    },
+  },
+});
 export default cartSlice.reducer;
-export const {
-    addItem,
-    removeItem,
-    increase,
-    decrease
-} = cartSlice.actions
+export const { addItem, removeItem, increase, decrease, checkout } =
+  cartSlice.actions;
--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,32 +1,32 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import api from "../../servises/config";
 import { productData } from "../../context/productData";
 const initialState = {
-    loading: false,
-    products: [],
-    error: ""
-}
-const fetchProducts = createAsyncThunk("product/fetchProducts" , ()=> {
-    // return api.get("/products")
-    return productData
-})
+  loading: false,
+  products: [],
+  error: "",
+};
+const fetchProducts = createAsyncThunk("product/fetchProducts", () => {
+  // return api.get("/products")
+
+  return productData;
+});
 const productSlice = createSlice({
-    name: "product",
-    initialState,
-    extraReducers: (builder) => {
-        builder.addCase(fetchProducts.pending, state=> {
-            state.loading = true;
-        })
-        builder.addCase(fetchProducts.fulfilled, (state, action)=> {
-            state.loading = false;
-            state.products = action.payload;
-        })
-        builder.addCase(fetchProducts.rejected, (state, action)=> {
-            state.loading = false;
-            state.products = [];
-            state.error = action.error.message;
-        })
-    }
-})
+  name: "product",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchProducts.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchProducts.fulfilled, (state, action) => {
+      state.loading = false;
+      state.products = action.payload;
+    });
+    builder.addCase(fetchProducts.rejected, (state, action) => {
+      state.loading = false;
+      state.products = [];
+      state.error = action.error.message;
+    });
+  },
+});
 export default productSlice.reducer;
 export { fetchProducts };
